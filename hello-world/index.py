201cb1b62958aa3aafdf70c5a6adb9cf267cc732
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-06-02 23:44:51 -0400

    added dictionaries; improved displaying messages; hello-world

--- hello-world/index.py
+++ hello-world/index.py
@@ -1,10 +1,12 @@
 # IF statements
+print("============ If Statements=============")
 a = 1
 b = 2
 print("bigger" if a > b else "smaller")
 
 # Lists
-student_names = ["Giorgi", "Jake", "Jenny", "Jessy", "Jason"]
+print("============ Lists =============")
+student_names = list(["Giorgi", "Jake", "Jenny", "Jessy", "Jason"])
 student_names.append("Mark")
 print(student_names[-3])
 print("Giorgi" in student_names)
@@ -13,9 +15,23 @@ print("Jake" not in student_names)
 print(student_names)
 print(student_names[1:])
 print(student_names[1:-2])
+
+# For Loops
+print("============ For Loops =============")
 for name in student_names:
     print(f"Loops student_names: {name}")
     print("With another formatter: {0}".format(name))
 
 for index in range(100, 200, 17):
     print("Index looping: {0}".format(index))
+
+# Dictionaries
+print("============ Dictionaries =============")
+student = {
+    "id": 1001,
+    "name": "Giorgi",
+    "nickname": "BoldEagle",
+    "age": None
+}
+print(student["name"])
+print(student.get("last_name", "No Last Name"))
