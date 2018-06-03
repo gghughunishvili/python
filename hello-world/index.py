cd78adc4751312ff40c504bba6cbafbfaeebfd06
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-06-02 23:25:59 -0400

    initial commit; hello-world

---
+++ hello-world/index.py
@@ -0,0 +1,15 @@
+# IF statements
+a = 1
+b = 2
+print("bigger" if a > b else "smaller")
+
+# Lists
+student_names = ["Giorgi", "Jake", "Jenny", "Jessy", "Jason"]
+student_names.append("Mark")
+print(student_names[-3])
+print("Giorgi" in student_names)
+del student_names[1]
+print("Jake" not in student_names)
+print(student_names)
+print(student_names[1:])
+print(student_names[1:-2])
