dd9b4c99fc493449dab7f0fdf8d1b34a23fa529e
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-06-03 22:54:46 -0400

    Added lambda functions and reading from file; hello-world

--- hello-world/functions.py
+++ hello-world/functions.py
@@ -1,7 +1,7 @@
 students = []
 
-def add_students(name, age, school):
-    students.append({"name": name, "age": age, "school": school})
+def add_student(name, age=0):
+    students.append({"name": name, "age": age})
 
 def var_args(name, *args):
     print(name)
@@ -11,8 +11,42 @@ def kargs(name, **kargs):
     print(name)
     print(kargs)
 
-add_students(name="Steve", age=29, school="Unknown")
+# Files
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception as e:
+        print(e)
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception as e:
+        print(e)
+
+
+
+add_student(name="Steve", age=29)
 print(students)
 
 var_args("test", "some", "other")
 kargs(name="steve", age=11, id=203032)
+
+# Inputs
+read_file()
+print(students)
+student_name = input("Enter student name:")
+student_age = input("Enter student age:")
+add_student(student_name, student_age)
+print(students)
+save_file(student_name)
+
+# Lambda Functions
+double = lambda x: x * 2
+num_to_double = int(input("Enter a number to double:"))
+print(double(num_to_double))
