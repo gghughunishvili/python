9d08dd17e72cfefadb4f5dac525ae0d6e5b554a7
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-06-05 00:28:30 -0400

    Added printing class itself; added static variable inside class example; hello-wrold

--- hello-world/oop.py
+++ hello-world/oop.py
@@ -1,10 +1,20 @@
 students = []
 
 class Student:
-    def add(self, name, id=101):
-        student = {"name": name, 'id': id}
-        students.append(student)
+    school_name = "Giorgi's Private School"
+    def __init__(self, name, id=101):
+        self.name = name
+        self.id = id
+        students.append(self)
 
-student = Student()
-student.add("Steve")
+    # Object to string
+    def __str__(self):
+        return "Student " + self.name;
+
+    def get_school_name(self):
+        return self.school_name
+
+student = Student("Steve")
+print(student)
 print(students)
+print(Student.school_name)
